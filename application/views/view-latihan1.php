d2cb0cc73a81490290234e0cc042977c0f107442
mrifaldik <muhamadrifaldi463@gmail.com>
2023-10-03 15:45:13 +0700

    Pertemuan 2

---
+++ application/views/view-latihan1.php
@@ -0,0 +1,15 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+
+<body>
+    Halo kawan.. Yuk kita belajar web programming..!!!<br>
+    Nilai 1 = <?= $n1; ?>
+    Nilai 2 = <?= $n2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+$n1 . " + " . $n2 . " = " . $hasil; ?>
+</body>
+
+</html>
